0e1246609eae098040480f29fb35974c14c3f5f4
Bùi Quang Huy <59251361+huybui76@users.noreply.github.com>
2023-06-12 18:02:16 +0700

    fix json

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,16 @@ export const App = () => (
     <Admin
         dataProvider={dataProvider}
     >
-        <Resource name="posts" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
-        <Resource name="comments" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
-        <Resource name="supplier" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="product" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="supplier" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="inventory" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="service" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="service_bill" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
-        <Resource name="inventory" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="reciept" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
-        <Resource name="sale" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="bill_of_sale" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="client" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="staff" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+       
     </Admin>
 );
 
